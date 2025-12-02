4bec08d381b8611c97fa28d496e93a762fe317ef
winturr <leanmangulabnan95@gmail.com>
2025-12-02 11:24:18 +0800

    login page implemented

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <section>
+      <div>
+        <Link href="/">Go to Home</Link>
+      </div>
+      <div>Login Page</div>
+    </section>
+  );
+}
