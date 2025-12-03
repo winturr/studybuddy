f5c6a9eb9fee47536dabb3cb4a82d4901ef972c0
winturr <leanmangulabnan95@gmail.com>
2025-12-03 09:10:58 +0800

    refactor: adjust padding and button styling in Header component for better layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,15 +4,15 @@ import Logo from "./Logo";
 export default function Header() {
   return (
     <header className="">
-      <div className="container flex items-center justify-between mx-auto p-5">
+      <div className="container flex items-center justify-between mx-auto p-3 sm:p-5">
         <Link href="/">
           <Logo />
         </Link>
-        <div className="flex gap-5">
-          <Link href="/login" className="button button-default">
+        <div className="flex gap-2 sm:gap-5">
+          <Link href="/login" className="button button-default text-sm sm:text-base py-2 px-3 sm:py-3 sm:px-5">
             Login
           </Link>
-          <Link href="/register" className="button button-default">
+          <Link href="/register" className="button button-default text-sm sm:text-base py-2 px-3 sm:py-3 sm:px-5">
             Sign Up
           </Link>
         </div>
