4a356c0f620f2eed5e457e31b5df07f6d95a6da4
winturr <leanmangulabnan95@gmail.com>
2025-12-03 10:07:00 +0800

    refactor: update font weight for improved visual hierarchy in Logo and Home components

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <div className="font-bold text-green-700 text-4xl tracking-[-2px]">
+    <div className="font-bold text-green-700 text-shadow-white text-4xl tracking-[-2px]">
       StudyBuddy
     </div>
   );
